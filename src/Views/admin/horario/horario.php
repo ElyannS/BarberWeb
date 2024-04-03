afb6a55cde459504aabae474682996fb0e3400c7
Elyann <ebfs49@gmail.com>
2024-04-03 12:00:06 -0300

    Update horario.php

--- src/Views/admin/horario/horario.php
+++ src/Views/admin/horario/horario.php
@@ -16,323 +16,39 @@
         <div class="lista">
             <table>
                 <tbody> 
-                    <tr>
-                        <td class="titulo_item">
-                            <div class="titulo-ser">
-                                Domingo
-                            </div>
-                            <div class="valor-ser">
-                                <?php 
-                                    $horarios = $data['informacoes']['lista'][0]['domingo'];
-                                    if($horarios === "Fechado"){
-                                        echo "Fechado";
-                                    } else{
-                                        $turno = explode(" - ", $horarios);
-                                        $turno1 = explode(", ", $turno[0]);
-                                        $turno2 = explode(", ", $turno[1]);
-    
-                                        $mostrar = " ";
-                                        if ($turno1 == " ") {
-                                            $mostrar = " ";
-                                        } else{
-                                            $mostrar = " às ";
+                    <?php foreach($data['informacoes']['lista'] as $horarios){?> 
+                        <tr>
+                            <td class="titulo_item">
+                                <div class="titulo-ser">
+                                    <?=$horarios['dia_semana']?>
+                                </div>
+                                <div class="valor-ser">
+                                    <?php 
+                                       if($horarios['turno1'] === 'FECHADO'){
+                                        echo 'Fechado';
+                                    } else {
+                                        $turno1 = $horarios['turno1'];
+                                        if(trim($turno1) === ""){
+                                            echo ' '; // Se não houver turno definido, mostra como "Fechado"
+                                        } else {
+                                            $primeiroUltimo = explode(", ", $turno1);    
+                                            echo $primeiroUltimo[0] . " às " . end($primeiroUltimo);
                                         }
-    
-                                        $mostrar2 = " ";
-                                        if ($turno2 == " ") {
-                                            $mostrar2 = " ";
-                                        } else{
-                                            $mostrar2 = " às ";
-                                        }
-                                        echo $turno1[0] . $mostrar . end($turno1) . "  ";
-    
-                                        echo $turno2[0] . $mostrar2 . end($turno2);
                                     }
-                                ?> 
-                               
-                            </div>
-                        </td>
-                        <td class="acao">
-                            <div class="btn">
-                                <a href="<?=URL_BASE?>admin/horarios-edit/">Editar</a>
-                            </div>
-                            <form action="<?=URL_BASE?>admin/horarios_delete" method="post">
-                                <input type="hidden" name="id" value="">
-                                <button type="submit"><i class="fa-solid fa-trash"></i></i></button>
-                            </form>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="titulo_item">
-                            <div class="titulo-ser">
-                                Segunda-Feira
-                            </div>
-                            <div class="valor-ser">
-                            <?php 
-                                $horarios = $data['informacoes']['lista'][0]['segunda'];
-                                if($horarios === "Fechado"){
-                                    echo "Fechado";
-                                } else{
-                                    $turno = explode(" - ", $horarios);
-                                    $turno1 = explode(", ", $turno[0]);
-                                    $turno2 = explode(", ", $turno[1]);
-
-                                    
-                                    $mostrar = " ";
-                                    if ($turno1 == false) {
-                                        $mostrar = " ";
-                                    } else{
-                                        $mostrar = " às ";
-                                    }
-
-                                    $mostrar2 = " ";
-                                    if ($turno2 == false) {
-                                        $mostrar2 = " ";
-                                    } else{
-                                        $mostrar2 = " às ";
-                                    }
-                                    echo $turno1[0] . $mostrar . end($turno1) . "  ";
-
-                                    echo $turno2[0] . $mostrar2 . end($turno2);
-                                }
-                            ?> 
-                            </div>
-                        </td>
-                        <td class="acao">
-                            <div class="btn">
-                                <a href="<?=URL_BASE?>admin/horarios-edit/">Editar</a>
-                            </div>
-                            <form action="<?=URL_BASE?>admin/horarios_delete" method="post">
-                                <input type="hidden" name="id" value="">
-                                <button type="submit"><i class="fa-solid fa-trash"></i></i></button>
-                            </form>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="titulo_item">
-                            <div class="titulo-ser">
-                                Terça-Feira
-                            </div>
-                            <div class="valor-ser">
-                            <?php 
-                                $horarios = $data['informacoes']['lista'][0]['terca'];
-                                if($horarios === "Fechado"){
-                                    echo "Fechado";
-                                } else{
-                                    $turno = explode(" - ", $horarios);
-                                    $turno1 = explode(", ", $turno[0]);
-                                    $turno2 = explode(", ", $turno[1]);
-
-                                    $mostrar = " ";
-                                    if ($turno1 == false) {
-                                        $mostrar = " ";
-                                    } else{
-                                        $mostrar = " às ";
-                                    }
-
-                                    $mostrar2 = " ";
-                                    if ($turno2 == false) {
-                                        $mostrar2 = " ";
-                                    } else{
-                                        $mostrar2 = " às ";
-                                    }
-                                    echo $turno1[0] . $mostrar . end($turno1) . "  ";
-
-                                    echo $turno2[0] . $mostrar2 . end($turno2);
-                                }
-                            ?> 
-                            </div>
-                        </td>
-                        <td class="acao">
-                            <div class="btn">
-                                <a href="<?=URL_BASE?>admin/horarios-edit/">Editar</a>
-                            </div>
-                            <form action="<?=URL_BASE?>admin/horarios_delete" method="post">
-                                <input type="hidden" name="id" value="">
-                                <button type="submit"><i class="fa-solid fa-trash"></i></i></button>
-                            </form>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="titulo_item">
-                            <div class="titulo-ser">
-                                Quarta-Feira
-                            </div>
-                            <div class="valor-ser">
-                            <?php 
-                                $horarios = $data['informacoes']['lista'][0]['quarta'];
-                                if($horarios === "Fechado"){
-                                    echo "Fechado";
-                                } else{
-                                    $turno = explode(" - ", $horarios);
-                                    $turno1 = explode(", ", $turno[0]);
-                                    $turno2 = explode(", ", $turno[1]);
-
-                                    $mostrar = " ";
-                                    if ($turno1 == " ") {
-                                        $mostrar = " ";
-                                    } else{
-                                        $mostrar = " às ";
-                                    }
-
-                                    $mostrar2 = " ";
-                                    if ($turno2 == " ") {
-                                        $mostrar2 = " ";
-                                    } else{
-                                        $mostrar2 = " às ";
-                                    }
-                                    echo $turno1[0] . $mostrar . end($turno1) . "  ";
-
-                                    echo $turno2[0] . $mostrar2 . end($turno2);
-                                }
-                            ?> 
-                            </div>
-                        </td>
-                        <td class="acao">
-                            <div class="btn">
-                                <a href="<?=URL_BASE?>admin/horarios-edit/">Editar</a>
-                            </div>
-                            <form action="<?=URL_BASE?>admin/horarios_delete" method="post">
-                                <input type="hidden" name="id" value="">
-                                <button type="submit"><i class="fa-solid fa-trash"></i></i></button>
-                            </form>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="titulo_item">
-                            <div class="titulo-ser">
-                                Quinta-Feira
-                            </div>
-                            <div class="valor-ser">
-                            <?php 
-                                $horarios = $data['informacoes']['lista'][0]['quinta'];
-                                if($horarios === "Fechado"){
-                                    echo "Fechado";
-                                } else{
-                                    $turno = explode(" - ", $horarios);
-                                    $turno1 = explode(", ", $turno[0]);
-                                    $turno2 = explode(", ", $turno[1]);
-
-                                    $mostrar = " ";
-                                    if ($turno1 == " ") {
-                                        $mostrar = " ";
-                                    } else{
-                                        $mostrar = " às ";
-                                    }
-
-                                    $mostrar2 = " ";
-                                    if ($turno2 == " ") {
-                                        $mostrar2 = " ";
-                                    } else{
-                                        $mostrar2 = " às ";
-                                    }
-                                    echo $turno1[0] . $mostrar . end($turno1) . "  ";
-
-                                    echo $turno2[0] . $mostrar2 . end($turno2);
-                                }
-                            ?> 
-                            </div>
-                        </td>
-                        <td class="acao">
-                            <div class="btn">
-                                <a href="<?=URL_BASE?>admin/horarios-edit/">Editar</a>
-                            </div>
-                            <form action="<?=URL_BASE?>admin/horarios_delete" method="post">
-                                <input type="hidden" name="id" value="">
-                                <button type="submit"><i class="fa-solid fa-trash"></i></i></button>
-                            </form>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="titulo_item">
-                            <div class="titulo-ser">
-                                Sexta-Feira
-                            </div>
-                            <div class="valor-ser">
-                            <?php 
-                                $horarios = $data['informacoes']['lista'][0]['sexta'];
-                                if($horarios === "Fechado"){
-                                    echo "Fechado";
-                                } else{
-                                    $turno = explode(" - ", $horarios);
-                                    $turno1 = explode(", ", $turno[0]);
-                                    $turno2 = explode(", ", $turno[1]);
-
-                                    $mostrar = " ";
-                                    if ($turno1 == " ") {
-                                        $mostrar = " ";
-                                    } else{
-                                        $mostrar = " às ";
-                                    }
-
-                                    $mostrar2 = " ";
-                                    if ($turno2 == " ") {
-                                        $mostrar2 = " ";
-                                    } else{
-                                        $mostrar2 = " às ";
-                                    }
-                                    echo $turno1[0] . $mostrar . end($turno1) . "  ";
-
-                                    echo $turno2[0] . $mostrar2 . end($turno2);
-                                }
-                            ?> 
-                            </div>
-                        </td>
-                        <td class="acao">
-                            <div class="btn">
-                                <a href="<?=URL_BASE?>admin/horarios-edit/">Editar</a>
-                            </div>
-                            <form action="<?=URL_BASE?>admin/horarios_delete" method="post">
-                                <input type="hidden" name="id" value="">
-                                <button type="submit"><i class="fa-solid fa-trash"></i></i></button>
-                            </form>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="titulo_item">
-                            <div class="titulo-ser">
-                                Sábado
-                            </div>
-                            <div class="valor-ser">
-                            <?php 
-                                $horarios = $data['informacoes']['lista'][0]['sabado'];
-                                if($horarios === "Fechado"){
-                                    echo "Fechado";
-                                } else{
-                                    $turno = explode(" - ", $horarios);
-                                    $turno1 = explode(", ", $turno[0]);
-                                    $turno2 = explode(", ", $turno[1]);
-
-                                    $mostrar = " ";
-                                    if ($turno1 == " ") {
-                                        $mostrar = " ";
-                                    } else{
-                                        $mostrar = " às ";
-                                    }
-
-                                    $mostrar2 = " ";
-                                    if ($turno2 == " ") {
-                                        $mostrar2 = " ";
-                                    } else{
-                                        $mostrar2 = " às ";
-                                    }
-                                    echo $turno1[0] . $mostrar . end($turno1) . "  ";
-
-                                    echo $turno2[0] . $mostrar2 . end($turno2);
-                                }
-                            ?> 
-                            </div>
-                        </td>
-                        <td class="acao">
-                            <div class="btn">
-                                <a href="<?=URL_BASE?>admin/horarios-edit/">Editar</a>
-                            </div>
-                            <form action="<?=URL_BASE?>admin/horarios_delete" method="post">
-                                <input type="hidden" name="id" value="">
-                                <button type="submit"><i class="fa-solid fa-trash"></i></i></button>
-                            </form>
-                        </td>
-                    </tr>
+                                    ?>
+                                </div>
+                            </td>
+                            <td class="acao">
+                                <div class="btn">
+                                    <a href="<?=URL_BASE?>admin/horarios-edit/">Editar</a>
+                                </div>
+                                <form action="<?=URL_BASE?>admin/horarios_delete" method="post">
+                                    <input type="hidden" name="id" value="">
+                                    <button type="submit"><i class="fa-solid fa-trash"></i></i></button>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php }?> 
                 </tbody>
             </table>
             <div class="paginacao">
