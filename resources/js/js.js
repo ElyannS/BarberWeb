9a4460fecbc1ca557310f7951408873fe7844238
Elyann Soares <100593501+ElyannS@users.noreply.github.com>
2024-07-28 23:56:36 -0300

    .

--- resources/js/js.js
+++ resources/js/js.js
@@ -172,23 +172,34 @@ $('.itemAgend').on('click', '#confirmarCancelar', function() {
     });
   }
   $('#btn-agendar').on('click' , function(){
-      $('.agenda-top .form').toggleClass('active');
+      $('.agenda-top .formA').toggleClass('active');
       $('body.admin .conteudo').addClass('back');
   });
-
+  $('#btn-bloquear').on('click' , function(){
+    $('.agenda-top .formB').toggleClass('activeBloquear');
+    $('body.admin .conteudo').addClass('back');
+  });
 
   $('.close').on('click' , function() {
     $('body.admin .container-popup').toggleClass('active');
     $('body.admin .conteudo').toggleClass('back');
   });
-
-  $('.close-form').on('click' , function() {
-    $('.agenda-top .form').toggleClass('active');
+  $('.close-formA').on('click' , function() {
+    $('.agenda-top .formA').toggleClass('active');
     $('body.admin .container-popup').toggleClass('active');
     $('body.admin .conteudo').toggleClass('back')
     var selectElement = $('#horariosDisponiveis');
     selectElement.empty();
   });
+
+  $('.close-formB').on('click' , function() {
+    $('.agenda-top .formB').toggleClass('active');
+    $('body.admin .container-popup').toggleClass('active');
+    $('body.admin .conteudo').toggleClass('back')
+    var selectElement = $('#horariosDisponiveis');
+    selectElement.empty();
+  });
+ 
 
 
   $('#selectHora1').on('change', function() {
--- src/Views/admin/agendamento/agendamentos.php
+++ src/Views/admin/agendamento/agendamentos.php
@@ -9,7 +9,7 @@
                     </div>
                     <div class="opcao-popup">
                         <a href="#" id="btn-agendar">Agendar</a>
-                        <a href="#">Bloquear</a>
+                        <a href="#" id="btn-bloquear">Bloquear</a>
                     </div>    
                 </div>
                 <div class="menu-agenda">
@@ -48,9 +48,9 @@
                 <div id="aviso">
 
                 </div>
-                <div class="form">
+                <div class="form formA">
                     <div class="close-popup">
-                        <i class="fa-solid fa-xmark close-form"></i>
+                        <i class="fa-solid fa-xmark close-formA"></i>
                     </div>
                     <form action="<?=URL_BASE?>admin/agendamentos_insert" class="form_ajax" id="form_create" method="POST" enctype="multipart/form-data">
                         <div class="row">
@@ -104,6 +104,53 @@
                         </div>
                         <div class="row">
                             <button type="submit" id="">Agendar</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="form formB">
+                    <div class="close-popup">
+                        <i class="fa-solid fa-xmark close-formB"></i>
+                    </div>
+                    <form action="<?=URL_BASE?>admin/agendamentos_insert" class="form_ajax" id="form_create" method="POST" enctype="multipart/form-data">
+                        <div class="row">
+                            <label>
+                                Nome Cliente
+                                <select  class="js-example-basic-single" name="id_cliente">
+                                    <option value="100006">Bloqueado</option>
+                                </select>
+                            </label>
+                        </div>
+                        <div class="row">
+                            <div class="w-80">
+                                <label>
+                                    Selecione o Tempo de Bloqueio
+                                    <select name="select_servico" id="tempo" required>
+                                        <option value="3">30 minutos</option>
+                                        <option value="2">1 hora</option>
+                                    </select>
+                                </label>
+                            </div>
+                            <div class="w-20">
+                                <label for="data">
+                                    Data
+                                    <input type="date" id="data" name="data">
+                                    <input type="hidden" id="data1" name="date">
+                                </label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <label>
+                                Horário
+                                <select name="time" id="horariosDisponiveis"></select>
+                            </label>
+                        </div>
+                        <div class="row">
+                            <label>
+                                <input name="select_barbeiro" id="selectBarbeiro" type="hidden">
+                            </label>
+                        </div>
+                        <div class="row">
+                            <button type="submit" id="">Bloquear</button>
                         </div>
                     </form>
                 </div>
